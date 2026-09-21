4ec83f2789082be30e60cb6b98f9afdf85640f59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-21 14:42:39 +0000

    solana 4.3.0

--- Formula/s/solana.rb
+++ Formula/s/solana.rb
@@ -1,8 +1,8 @@
 class Solana < Formula
   desc "Web-Scale Blockchain for decentralized apps and marketplaces"
   homepage "https://www.anza.xyz/"
-  url "https://github.com/anza-xyz/agave/archive/refs/tags/v4.2.2.tar.gz"
-  sha256 "6ae81fb5657beb5fbe8c7bb83e6f0794a7a46c10a05c0a36bbb12e76579c7f50"
+  url "https://github.com/anza-xyz/agave/archive/refs/tags/v4.3.0.tar.gz"
+  sha256 "a1ff6f5aee5702c53feac14f7fa82209a61a7a3cbaa977148b06051fad7a72fa"
   license "Apache-2.0"
   version_scheme 1
 
